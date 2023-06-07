0d864effa53f7b7f77d31b42c902b6310b55283b
aaddhhi <131011274+aaddhhi@users.noreply.github.com>
2023-06-07 11:17:07 +0530

    Add files via upload

---
+++ 19thquestion.py
@@ -0,0 +1,9 @@
+start_table = int(input("Enter the start table: "))
+end_table = int(input("Enter the end table: "))
+
+print("Multiplication tables from", start_table, "to", end_table)
+for i in range(1, 11):
+    for j in range(start_table, end_table + 1):
+        result = j * i
+        print(j, "*", i, "=", result)
+    print()
